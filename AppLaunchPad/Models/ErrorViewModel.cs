4fdfd35e12f11c3a0b13a133cdb8c4fcad5bf349
Chuck Randles <Rac357373@gmail.com>
2022-08-28 21:07:45 -0500

    Initial commit

---
+++ AppLaunchPad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppLaunchPad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
